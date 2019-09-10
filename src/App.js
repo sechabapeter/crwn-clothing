05c244b8774568508b31eb9101e3c0fc3c411517
sechabapeter <49229010+sechabapeter@users.noreply.github.com>
2019-09-10 15:19:41 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -50,7 +50,7 @@ render() {
       <Switch>
       <Route exact path='/' component={HomePage}/>
      <Route  path='/shop' component={ShopPage}/>
-     <Route  path='/signIn' component={SignInAndSignUpPage}/>
+     <Route exact path='/signIn' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
      </Switch>
      </BrowserRouter>
        
@@ -61,10 +61,15 @@ render() {
 
 }
 }
+const mapStateToProps = ({user}) => ({
+
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
 
     setCurrentUser: user => dispatch(setCurrentUser(user))
 
 });
 
-export default connect(null, mapDispatchToProps) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
