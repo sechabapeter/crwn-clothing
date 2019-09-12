fd1f1ab7da1d6db7ab362b80c052581e41f9c996
sechabapeter <49229010+sechabapeter@users.noreply.github.com>
2019-09-12 09:36:34 +0200

    added item and remove item funtionality

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,10 +1,10 @@
 import  React from 'react';
 import './checkout-item.styles.scss';
 import { connect } from  'react-redux';
-import { clearItemsFromCart } from '../../redux/cart/cart.action';
+import { clearItemsFromCart, addItem, removeItem } from '../../redux/cart/cart.action';
 
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const {name, imageUrl, price, quantity } = cartItem;
     return (
     <div className='checkout-item'>
@@ -12,12 +12,19 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
             <img  src={imageUrl} alt='item'  />
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
+        <span className='quantity'>
+        <div className='arrow' onClick= {() => removeItem(cartItem)}>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+        </span>
         <span className='price'>{price}</span>
         <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
 )};
 const mapDispatchToProps = dispatch => ({
-    clearItem:  item => dispatch(clearItemsFromCart(item))
+    clearItem:  item => dispatch(clearItemsFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+
 });
 export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -10,6 +10,11 @@ export const addItem = item => ({
     payload: item
 });
 
+export const removeItem = item => ({
+    type: CartActionTypes.REOMVE_ITEM,
+    payload: item
+});
+
 export const clearItemsFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
